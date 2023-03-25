c72f1dea48eca464ecd89220dfa79983fd421d8c
does <ldk980130@gmail.com>
2023-03-25 13:53:52 +0900

    feat: init

---
+++ src/main/java/com/Kyukeoton/slowerlearner/SlowerLearnerApplication.java
@@ -0,0 +1,13 @@
+package com.Kyukeoton.slowerlearner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowerLearnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowerLearnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kyukeoton/slowerlearner/SlowerLearnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kyukeoton.slowerlearner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowerLearnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
