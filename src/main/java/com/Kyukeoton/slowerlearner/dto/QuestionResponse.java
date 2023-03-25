e45b6d99804b22778332d28f80e5fd0d1358b6e5
does <ldk980130@gmail.com>
2023-03-25 23:03:51 +0900

    fix: api 형식 수정

--- src/main/java/com/Kyukeoton/slowerlearner/dto/QuestionResponse.java
+++ src/main/java/com/Kyukeoton/slowerlearner/dto/QuestionResponse.java
@@ -1,26 +1,57 @@
 package com.Kyukeoton.slowerlearner.dto;
 
+import com.Kyukeoton.slowerlearner.domain.Answer;
 import com.Kyukeoton.slowerlearner.domain.Question;
-import lombok.AllArgsConstructor;
+import com.Kyukeoton.slowerlearner.domain.QuestionImage;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 public class QuestionResponse {
 
     private Long questionId;
     private String content;
-    private QuestionImageResponses questionImages;
-    private AnswerResponses answers;
+    private List<QuestionImageResponse> questionImages;
+    private List<AnswerResponse> answers;
 
     public QuestionResponse(Question question) {
         this.questionId = question.getId();
         this.content = question.getContent();
-        this.questionImages = new QuestionImageResponses(question.getQuestionImages());
-        this.answers = new AnswerResponses(question.getAnswers());
+        this.questionImages = question.getQuestionImages()
+                .stream()
+                .map(QuestionImageResponse::new)
+                .collect(Collectors.toList());
+        this.answers = question.getAnswers()
+                .stream()
+                .map(AnswerResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    @NoArgsConstructor
+    @Getter
+    private static class QuestionImageResponse {
+        private String name;
+        private String url;
+
+        public QuestionImageResponse(QuestionImage questionImage) {
+            this.name = questionImage.getName();
+            this.url = questionImage.getUrl();
+        }
+    }
+
+    @NoArgsConstructor
+    @Getter
+    private static class AnswerResponse {
+        private String content;
+        private Boolean isCorrect;
+
+        public AnswerResponse(Answer answer) {
+            this.content = answer.getContent();
+            this.isCorrect = answer.getIsCorrect();
+        }
     }
 }
